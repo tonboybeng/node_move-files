41c727d9a8925954d7ba4891176b1e189a045b99
Toni_Adrenalin <toni.toni@adrenalinmedia.com.au>
2025-03-14 21:11:20 +1100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
 // write code here
+const fs = require('fs');
+
+const path = require('path');
+
+function moveFile(sourcePath, destinationPath) {
+  try {
+    if (
+      fs.existsSync(destinationPath) &&
+      fs.lstatSync(destinationPath).isDirectory()
+    ) {
+      const fileName = path.basename(sourcePath);
+
+      fs.renameSync(sourcePath, path.join(destinationPath, fileName));
+    } else {
+      fs.renameSync(sourcePath, destinationPath);
+    }
+
+    console.log(
+      `Successful moving file from: ${sourcePath} to: ${destinationPath}`,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function main() {
+  if (process.argv.length !== 4) {
+    console.log(
+      'Invalid arguments. E.g. node src/app.js {sourcePath} {destinationPath}',
+    );
+  }
+
+  const sourcePath = process.argv[2];
+  const destinationPath = process.argv[3];
+
+  moveFile(sourcePath, destinationPath);
+}
+
+main();
